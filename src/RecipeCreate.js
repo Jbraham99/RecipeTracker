0ed675d97a62766190f708e4d6d1cb3a11a319bf
Jbraham99 <braham.joseph15@gmail.com>
2023-08-08 13:40:36 -0700

    fixed: can't add empty recipe

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -26,11 +26,11 @@ function RecipeCreate({setRecipes, recipes}) {
     <form onSubmit={submitHandler} name="create">        
         <table>
             <tr>
-                <td><input type="text" name="name" placeholder="Name" onChange={changeHandler} value={recipe.name}/></td>
-                <td><input type="text" name="cuisine" placeholder="Cuisine" onChange={changeHandler} value={recipe.cuisine}/></td>
-                <td><input type="url" name="photo" placeholder="URL" onChange={changeHandler} value={recipe.photo}/></td>
-                <td><textarea type="textarea" name="ingredients" placeholder="Ingredients" onChange={changeHandler} value={recipe.ingredients}></textarea></td>
-                <td><textarea type="textarea" name="preparation" placeholder="Preparation" onChange={changeHandler} value={recipe.preparation}></textarea></td>
+                <td><input type="text" name="name" placeholder="Name" onChange={changeHandler} value={recipe.name} required/></td>
+                <td><input type="text" name="cuisine" placeholder="Cuisine" onChange={changeHandler} value={recipe.cuisine} required/></td>
+                <td><input type="url" name="photo" placeholder="URL" onChange={changeHandler} value={recipe.photo} required/></td>
+                <td><textarea type="textarea" name="ingredients" placeholder="Ingredients" onChange={changeHandler} value={recipe.ingredients} required></textarea></td>
+                <td><textarea type="textarea" name="preparation" placeholder="Preparation" onChange={changeHandler} value={recipe.preparation} required></textarea></td>
                 <td><button type="submit">Create</button></td>
             </tr>
         </table>
